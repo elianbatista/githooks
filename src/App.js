72fdfdfd78920688a939564ca25e02417b58abc2
Elian Batista <elianbatista2000@gmail.com>
2020-12-23 21:25:54 -0300

    utilizando use effect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(async () => {
+    const response = await fetch('https://api.github.com/users/elianbatista/repos')
+    const data = await response.json()
+
+    setRepositories(data)
+  }, [])
+
+  function handleFavorite(id) {
+    
+  }
+
   return (
-    <div className="App" />
+    <>
+      <ul>
+        {
+          repositories.map(repo => (
+            <li key="{repo.id}">
+              {repo.name}
+              <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+            </li>
+          )
+        )}
+      </ul>
+    </>
   );
 }
 
